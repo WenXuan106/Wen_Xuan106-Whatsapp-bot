74c86ea7ba8ea539667962434f901a719e7dea0e
Wen_Xuan106 <yeewenxuan106@gmail.com>
2026-09-19 23:19:03 +0800

    Add phrases to ash command responses
    Added new phrases to the ash command responses.

--- commands/ash.js
+++ commands/ash.js
@@ -8,6 +8,13 @@ const SENTENCES = [
   "I CAME ALL THE WAY FROM CHINA ON TWO BICYCLE WHEEL TO SEE THIS",
   "🏞️ IS THIS A PARK OR A PLAYGROUND??",
   "AYD!!!",
+  "U THINK WE SCOLD U VV HAPPY AH",
+  "📢 *YALL KNOW PPL LEAVING CAUSE YALL SPAM EVERYDAY AND CHAT IS TAKING UP AROUND 1 - 10GB*",
+  "SON",
+  "WAH",
+  "HOW ARE THERE RARITIES-",
+  "SHUCKS",
+  "6583774280@s.whatsapp.net using bot for a few months and no bans (the only ban is when he added 100+ ppl in a gc)",
 ];
 
 module.exports = {
